4956c1b7cc6236f2e65bbc79b1b5b75745329233
DDeShon <36036284+DDeShon@users.noreply.github.com>
2021-11-11 09:38:24 -0600

    setting up bindings for gui

--- main.py
+++ main.py
@@ -9,6 +9,27 @@ class MyGUI(QMainWindow):
         uic.loadUi("todogui.ui", self)
         self.show()
 
+        self.setWindowTitle("Todo List v1.0.0")
+        self.model = QStandardItemModel()
+        self.listView.setModel(self.model)
+
+        self.plusButton.clicked.connect(self.add_item)
+        self.minusButton.clicked.connect(self.remove_item)
+        self.actionLoad.triggered.connect(self.open_file)
+        self.actionSave.triggered.connect(self.save_file)
+
+    def add_item(self):
+        print("Add")
+
+    def remove_item(self):
+        print("Remove")
+
+    def open_file(self):
+        print("Open")
+
+    def save_file(self):
+        print("Save")
+
 app = QApplication([])
 window = MyGUI()
 app.exec_()
