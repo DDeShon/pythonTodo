a35465d49fdacd1ba953f01667dc813998e5babe
DDeShon <36036284+DDeShon@users.noreply.github.com>
2021-11-11 09:52:55 -0600

    set up add and remove functionality

--- main.py
+++ main.py
@@ -19,10 +19,23 @@ class MyGUI(QMainWindow):
         self.actionSave.triggered.connect(self.save_file)
 
     def add_item(self):
-        print("Add")
+        item, confirmed = QInputDialog.getText(self, "Add Item", "New Item:", QLineEdit.Normal, "")
+
+        if confirmed and not item.isspace():
+            item = QStandardItem(item)
+            self.model.appendRow(item)
 
     def remove_item(self):
-        print("Remove")
+        if len(self.listView.selectedIndexes()) != 0:
+            selected = self.listView.selectedIndexes()[0]
+
+            dialog = QMessageBox()
+            dialog.setText(f"Are you sure you want to remove '{selected.data()}'?")
+            dialog.addButton(QPushButton("Yes"), QMessageBox.YesRole)
+            dialog.addButton(QPushButton("No"), QMessageBox.NoRole)
+
+            if dialog.exec_() == 0:
+                self.model.removeRow(selected.row())
 
     def open_file(self):
         print("Open")
